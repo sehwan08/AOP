43d1f6ffceeaf1ed4ee28f9669c1711999697128
sehwan08 <sehwan_8@naver.com>
2021-09-19 20:20:45 +0900

    Finalized AOP

--- src/main/java/com/cos/aop/config/BindingAdvice.java
+++ src/main/java/com/cos/aop/config/BindingAdvice.java
@@ -4,15 +4,21 @@ package com.cos.aop.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.cos.aop.domain.CommonDto;
 
@@ -20,8 +26,17 @@ import com.cos.aop.domain.CommonDto;
 @Aspect
 public class BindingAdvice {
 	
+	private static final Logger log = LoggerFactory.getLogger(BindingAdvice.class);
+	
 	@Before("execution(* com.cos.aop.web..*Controller.*(..))")
 	public void beforeTest() {
+		
+		
+		HttpServletRequest request =
+				((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes())
+				.getRequest();
+		System.out.println("주소: "+request.getRequestURL());
+		
 		System.out.println("Before-Log saved.");
 	}
 	
@@ -51,6 +66,8 @@ public class BindingAdvice {
 					
 					for(FieldError error:bindingResult.getFieldErrors()) {
 						errorMap.put(error.getField(), error.getDefaultMessage());
+						log.warn(type+"."+method+"() => 필드 :"+error.getField()+", 메시지: "+error.getDefaultMessage());
+						log.debug(type+"."+method+"() => 필드 :"+error.getField()+", 메시지: "+error.getDefaultMessage());
 					}
 					
 					return new CommonDto<>(HttpStatus.BAD_REQUEST.value(),errorMap);
